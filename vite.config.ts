c3f52126931e2ddcf2344154069295a8c1ea5685
designernote <designernote@naver.com>
2024-10-20 15:55:55 +0900

    22222222

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/dfdfdf
+// https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/nana/",
+  base: "/nana/",
   plugins: [react({})],
   resolve: {
     alias: {
